fe47b78278a597dd53685af3931bf8616ff726e3
bridget462 <bridges.dev.create@gmail.com>
2021-09-21 17:53:31 +0900

    sign in btn now refactored to use hook useState

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey =
+    "pk_test_51JZtCuAVrdJ2OpdawjQa4kfMJP00IOTGa3wvl3LLriYm0Hnm4I07Lk0i6YBA5JrE8Ua2hAITAPUuVI6efeTLCEP500TH9lU0bY";
 
-  const onToken = token => {
+  const onToken = (token) => {
     console.log(token);
-    alert('Payment Succesful!');
+    alert("Payment Succesful!");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
